f4d738460489fcd9ff6539b3b2a2ed335eaa511a
memon-gulam-45 <memongulam45@gmail.com>
2025-12-07 01:50:38 +0530

    Add marker functionality and custom markers to the map

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,18 @@
-const map = L.map("map").setView([22.4836, 72.8014], 13);
+coordinates.reverse();
+
+const map = L.map("map").setView(coordinates, 13);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
 }).addTo(map);
 
-L.marker([22.4836, 72.8014]).addTo(map).bindPopup("Petlad");
+let customIcon = L.icon({
+  iconUrl: "../pin.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
+L.marker(coordinates, { icon: customIcon }).addTo(map);
+
+console.log(coordinates);
